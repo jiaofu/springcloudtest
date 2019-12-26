e06107b69eb25c815417eda01f206ee774188932
shuiliu <zhangshuiliu@hotmail.com>
2019-12-27 00:14:58 +0800

    发现服务,但是报错

---
+++ feignclient2/src/test/java/com/springcloud/test/feignclient2/Feignclient2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.test.feignclient2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Feignclient2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ feignweb/src/test/java/com/springcloud/test/feignweb/FeignwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.test.feignweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
